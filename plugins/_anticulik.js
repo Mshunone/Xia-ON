e7fc4106eb7a14bbb0bf01bc602901ce462a0c2a
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-24 21:47:31 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,7 +5,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `「 WH-MODS-BOT-V1 」
+    let teks = `「 SaxiaBotzV1 」
 
     ╭━━━━「 SEWA 」
     ┊⫹⫺ Hemat: 5k/grup (1 minggu)
@@ -16,26 +16,24 @@ handler.all = async function (m, { isBlocked }) {
     ╰═┅═━––––––๑
     
     ╭━━━━「 PREMIUM 」
-    ┊⫹⫺ Hemat: 5k (1 minggu)
-    ┊⫹⫺ Normal: 20k (1 bulan)
-    ┊⫹⫺ Pro: 40k (4 bulan)
-    ┊⫹⫺ Vip: 50k (8 bulan)                                               
-    ┊⫹⫺ Permanent: = 70k (Unlimited)
+    ┊⫹⫺ Hemat: 5k (1 Bulan)
+    ┊⫹⫺ Normal: 15k (2/5 bulan)
+    ┊⫹⫺ Pro: 25k (4 bulan)
+    ┊⫹⫺ Vip: 35k (8 bulan)                                               
+    ┊⫹⫺ Permanent: = 100k (Unlimited)
     ╰═┅═━––––––๑
     
     ⫹⫺ PAYMENT:
-    • Pulsa Telkomsel: [082127487538]
-    • Dana: [082127487538]
-    • Gopay: [082127487538]
-    • Ovo: [082127487538]
-    • Link Aja: [082127487538]
+    • Pulsa : [Ga dulu]
+    • Shopay: [---]
+    • Gopay: [---]
     
     Nomor Owner :
-    wa.me/6282127487538
+    wa.me/6283805685278
     
     ▌│█║▌║▌║║▌║▌║█│▌
     
-    #BY WH MODS DEV`
+    #SaxiaBotV1`
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
     this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
