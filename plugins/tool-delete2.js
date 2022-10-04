07d976f7824e56cd039517c863d8b0394623eb79
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-04 21:36:17 +0700

    Update and rename _del2.js to tool-delete2.js

--- plugins/tool-delete2.js
+++ plugins/tool-delete2.js
@@ -2,6 +2,6 @@ let handler = async (m, { conn, command, text }) => {
 await conn.sendMessage(m.chat, { delete: m.key })
 }
 handler.customPrefix = /chat.whatsapp.com/i
-handler.command = new RegExp
+handler.command = /chat.whatsapp.com/i
 
 module.exports = handler
