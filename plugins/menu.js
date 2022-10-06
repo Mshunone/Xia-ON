ee870c8c9d1ced4592dc904d04fd9a4c3387e5e6
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-06 20:50:23 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -273,9 +273,9 @@ let tek = `*${ucapan()} ${conn.getName(m.sender)}*
 ┊✗• *ᴘʀᴇᴍɪᴜᴍ:* ${usrs.premiumTime > 1 ? 'Yes': 'No'}
 ┊✗• *ᴜsᴇʀs:* ${Object.keys(global.db.data.users).length}
 ┊✗• *ʟɪᴍɪᴛ:* ${usrs.limit}
-┊✗• *ʀᴏʟᴇ:* ${usrs.role}${usrs.premiumTime > 1 ? `
 ┊✗• *ʟᴇᴠᴇʟ:* ${usrs.level}
 ┗━━━━━━━━━━━━━━━┈─❏
+✗> *ʀᴏʟᴇ:* ${usrs.role}${usrs.premiumTime > 1 ? `
 ┊↬✗• *ᴇxᴘɪʀᴇᴅ ᴘʀᴇᴍɪᴜᴍ:*
 ${clockStringP(usrs.premiumTime - new Date())}` : ''}
 `
